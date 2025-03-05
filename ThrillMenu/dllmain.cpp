dbc744e3c55ab92f9a1cb8277e6e278facecc15d
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-03-05 19:58:11 +0100

    Add original thrillmenu code

---
+++ ThrillMenu/dllmain.cpp
@@ -0,0 +1,67 @@
+﻿#include <Windows.h>
+#include <thread>
+#include <cstdint>
+#include <iostream>
+#include "hooks/hooks.h"
+
+void Setup(const HINSTANCE instance) {
+    try
+    {
+        AllocConsole();
+        FILE* fDummy;
+        freopen_s(&fDummy, "CONIN$", "r", stdin);
+        freopen_s(&fDummy, "CONOUT$", "w", stderr);
+        freopen_s(&fDummy, "CONOUT$", "w", stdout);
+        gui::Setup();
+        hooks::Setup();
+        std::cout << "Setup initialized!" << std::endl;
+    }
+    catch (const std::exception& error)
+    {
+        MessageBeep(MB_ICONERROR);
+        MessageBox(
+            0,
+            error.what(),
+            "Thrill menu error",
+            MB_OK | MB_ICONEXCLAMATION
+        );
+
+        goto UNLOAD;
+    }
+    while (!GetAsyncKeyState(VK_END))
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+UNLOAD:
+    hooks::Destroy();
+    gui::Destroy();
+
+    FreeLibraryAndExitThread(instance, 0);
+}
+
+BOOL WINAPI DllMain(
+    HINSTANCE instance,  // handle to DLL module
+    DWORD reason,     // reason for calling function
+    LPVOID lpvReserved)  // reserved
+{
+    if (reason == DLL_PROCESS_ATTACH) {
+        //MessageBox(0, "Content", "Title", MB_OK);
+
+        
+        // Print debug information to the new console
+        DisableThreadLibraryCalls(instance);
+
+        const auto thread = CreateThread(
+            nullptr,
+            0,
+            reinterpret_cast<LPTHREAD_START_ROUTINE>(Setup),
+            instance,
+            0,
+            nullptr
+        );
+        if (thread) {
+            CloseHandle(thread);
+        }
+    }
+    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
---
+++ ThrillMenu/gui/gui.cpp
@@ -0,0 +1,286 @@
+#include "gui.h"
+#include "../ext/imgui/imgui.h"
+#include "../ext/imgui/imgui_impl_win32.h"
+#include "../ext/imgui/imgui_impl_dx9.h"
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <iostream>
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(
+	HWND window,
+	UINT message,
+	WPARAM wideParam,
+	LPARAM longParam
+);
+
+//window process
+LRESULT CALLBACK WindowProcess(
+	HWND window,
+	UINT message,
+	WPARAM wideParam,
+	LPARAM longParam
+);
+
+bool gui::SetupWindowClass(const char* windowClassName) noexcept
+{
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = DefWindowProc;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = GetModuleHandle(NULL);
+	windowClass.hIcon = NULL;
+	windowClass.hCursor = NULL;
+	windowClass.hbrBackground = NULL;
+	windowClass.lpszMenuName = NULL;
+	windowClass.lpszClassName = windowClassName;
+	windowClass.hIconSm = NULL;
+
+	//register
+	if (!RegisterClassEx(&windowClass))
+	{
+		return false;
+	}
+	std::cout << "Setup window class!" << std::endl;
+	return true;
+}
+void gui::DestroyWindowClass() noexcept
+{
+	UnregisterClass(
+		windowClass.lpszClassName,
+		windowClass.hInstance
+	);
+}
+
+bool gui::SetupWindow(const char* windowName) noexcept
+{
+	//create temp window
+	window = CreateWindow(
+		windowClass.lpszClassName,
+		windowName,
+		WS_OVERLAPPEDWINDOW,
+		0,
+		0,
+		100,
+		100,
+		0,
+		0,
+		windowClass.hInstance,
+		0
+	);
+	if (!window)
+	{
+		return false;
+	}
+	std::cout << "Setup window" << std::endl;
+	return true;
+}
+void gui::DestroyWindow() noexcept
+{
+	if (window)
+	{
+		DestroyWindow(window);
+	}
+}
+
+bool gui::SetupDirectX() noexcept
+{
+	const auto handle = GetModuleHandle("d3d9.dll");
+
+	if (!handle)
+	{
+		return false;
+	}
+	using CreateFn = LPDIRECT3D9(__stdcall*)(UINT);
+	const auto create = reinterpret_cast<CreateFn>(GetProcAddress(
+		handle,
+		"Direct3DCreate9"
+	));
+	if (!create)
+	{
+		return false;
+	}
+
+	d3d9 = create(D3D_SDK_VERSION);
+
+	if (!d3d9)
+	{
+		return false;
+	}
+
+	D3DPRESENT_PARAMETERS params = {};
+	params.BackBufferWidth = 0;
+	params.BackBufferHeight = 0;
+	params.BackBufferFormat = D3DFMT_UNKNOWN;
+	params.BackBufferCount = 0;
+	params.MultiSampleType = D3DMULTISAMPLE_NONE;
+	params.MultiSampleQuality = NULL;
+	params.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	params.hDeviceWindow = window;
+	params.EnableAutoDepthStencil = 0;
+	params.AutoDepthStencilFormat = D3DFMT_UNKNOWN;
+	params.Flags = NULL;
+	params.FullScreen_RefreshRateInHz = 0;
+	params.PresentationInterval = 0;
+
+	if (d3d9->CreateDevice(
+		D3DADAPTER_DEFAULT,
+		D3DDEVTYPE_NULLREF,
+		window,
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING | D3DCREATE_DISABLE_DRIVER_MANAGEMENT,
+		&params,
+		&device
+	) < 0)
+	{
+		return false;
+	}
+	std::cout << "Setup DirectX!" << std::endl;
+	return true;
+}
+void gui::DestroyDirectX() noexcept
+{
+	if (device)
+	{
+		device->Release();
+		device = NULL;
+	}
+	if (d3d9)
+	{
+		d3d9->Release();
+		d3d9 = NULL;
+	}
+}
+
+//setup device
+void gui::Setup()
+{
+	if (!SetupWindowClass("ThrillMenuClass"))
+	{
+		std::cout << "classerror" << std::endl;
+		throw std::runtime_error("Failed to create window class.");
+	}
+	if (!SetupWindow("Thrill Menu"))
+	{
+		throw std::runtime_error("Failed to create window.");
+		std::cout << "windowerrer!" << std::endl;
+	}
+	if (!SetupDirectX())
+	{
+		std::cout << "dxerror" << std::endl;
+		throw std::runtime_error("Failed to create device");
+	}
+
+	DestroyWindow();
+	DestroyWindowClass();
+}
+
+void gui::SetupMenu(LPDIRECT3DDEVICE9 device) noexcept
+{
+	auto params = D3DDEVICE_CREATION_PARAMETERS{};
+	device->GetCreationParameters(&params);
+
+	window = params.hFocusWindow;
+
+	originalWindowProcesss = reinterpret_cast<WNDPROC>(
+		SetWindowLongPtr(window, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(WindowProcess))
+		);
+
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX9_Init(device);
+
+	setup = true;
+	std::cout << "Setup ImGui" << std::endl;
+}
+void gui::Destroy() noexcept
+{
+	ImGui_ImplWin32_Shutdown();
+	ImGui_ImplDX9_Shutdown();
+	ImGui::DestroyContext();
+
+	SetWindowLongPtr(
+		window,
+		GWLP_WNDPROC,
+		reinterpret_cast<LONG_PTR>(originalWindowProcesss)
+	);
+
+	DestroyDirectX();
+}
+void gui::Render() noexcept
+{
+	RECT rect;
+	GetClientRect(window, &rect);
+
+	POINT ul;
+	ul.x = rect.left;
+	ul.y = rect.top;
+
+	POINT lr;
+	lr.x = rect.right;
+	lr.y = rect.bottom;
+
+	MapWindowPoints(window, nullptr, &ul, 1);
+	MapWindowPoints(window, nullptr, &lr, 1);
+
+	rect.left = ul.x;
+	rect.top = ul.y;
+
+	rect.right = lr.x;
+	rect.bottom = lr.y;
+
+	ClipCursor(&rect);
+
+	POINT p;
+	if (GetCursorPos(&p))
+	{
+		SetCursorPos(p.x, p.y);
+	}
+
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("Thrill Menu", &open);
+	//ImGui::Text("%.2X", money);
+	ImGui::Text("Money:");
+	ImGui::End();
+
+	ImGui::EndFrame();
+	ImGui::Render();
+	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+	std::cout << "Render imgui" << std::endl;
+}
+
+LRESULT CALLBACK WindowProcess(
+	HWND window,
+	UINT message,
+	WPARAM wideParam,
+	LPARAM longParam
+)
+{
+	//togglemenu
+	if (GetAsyncKeyState(VK_INSERT) & 1)
+	{
+		gui::open = !gui::open;
+	}
+	if (gui::open && ImGui_ImplWin32_WndProcHandler(
+		window,
+		message,
+		wideParam,
+		longParam
+	))
+	{
+		return 1L;
+	}
+
+	return CallWindowProc(
+		gui::originalWindowProcesss,
+		window,
+		message,
+		wideParam,
+		longParam
+	);
+}
